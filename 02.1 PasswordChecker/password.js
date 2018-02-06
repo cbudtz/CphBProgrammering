5451c87d5aa16ed97aaa36c916fe64ae5acfe2c2
cbudtz <chrbudtz@gmail.com>
2018-02-06 23:47:49 +0100

    Kodeskabeloner

---
+++ 02.1 PasswordChecker/password.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Christian on 06-02-2018.
+ */
+let kodeord = prompt("Indtast dit kodeord");
+
+//TODO kontroller kodeordets længde!
+
---
+++ 02.2 Badmintonfest/badminton.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Christian on 06-02-2018.
+ */
+let alder = Number(prompt("Indtast din alder"));
+let costume = prompt("Skriv din udklædning");
+
+//TODO udtænk det rigtige udtryk.
+if("check udklædning og alder") {
+    console.log("Du må gerne komme ind");
+} else {
+    console.log("Du må ikke komme ind");
+}
