74b941398cb1aa681388da2d26431fff07e3ef3f
cbudtz <chrbudtz@gmail.com>
2018-02-09 00:05:15 +0100

    lektion 2 commit

---
+++ 02 Snemand/script.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Christian on 08-02-2018.
+ */
+
+let temperature = Number(prompt("Indtast temperaturen:"));
+let sunshine = confirm("Lad solen skinne?")
+
+
+//TODO: Tegn To cirkler som snemandens nederstebolde
+circle(50,300,30); // Sådan kunne den nederste se ud
+
+
+//TODO: Tegn hovedet med face() funktionen:
+
+function face(x,y,radius){
+    //Vi kan lige så godt bruge vores cirkel funktion til at tegne cirkel om ansigtet
+    circle(x,y);
+    let canvas = document.getElementById("snowcanvas").getContext('2d');
+    //TODO: Tegn øjne
+
+    //TODO: Tegn glad mund hvis det er rart at være snemand eller sur mund, hvis det er surt.
+    //Funktionen findes allerede - se nederst
+
+}
+
+function circle(x,y, radius){
+    let canvas = document.getElementById("snowcanvas").getContext('2d');
+    //TODO: draw a circle
+
+}
+
+function hat(x,y){
+    let canvas = document.getElementById("snowcanvas").getContext('2d');
+    //TODO: Tegn eventuelt snemandends hat se: https://www.w3schools.com/tags/canvas_rect.asp
+}
+/**
+ *
+ * @param x - angiver x placeringen
+ * @param y - angiver y placeringen
+ * @param size - angiver mundens bredde
+ * @param happy - angiver om det er en glad eller sur mund
+ */
+function mouth(x,y, size, happy){
+    let canvas = document.getElementById("snowcanvas").getContext('2d');
+    canvas.beginPath();
+    if (happy) {
+        canvas.arc(x,y, size,(1/4)*Math.PI,(3/4)*Math.PI)
+    } else {
+        canvas.arc(x,y+(5/3)*size,size,(-3/4)*Math.PI,(-1/4)*Math.PI)
+    }
+    canvas.stroke();
+}
---
+++ 03 Firepaastribebraet/fps.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Christian on 07-02-2018.
+ */
+
+let x = Number(prompt("Hvor skal der være en brik? Indtast X: "));
+let y = Number(prompt("Hvor skal der være en brik? Indtast Y: "));
+
+function drawBoardWithPieceAt(x, y) {
+
+    //TODO Skriv noget kode, der kan tegne et 'fire på stribe' bræt;
+
+}
