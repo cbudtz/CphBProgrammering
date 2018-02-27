fc5983c3b787fae9d90272e1a16648097b918c0a
Christian Budtz <chrbudtz@gmail.com>
2018-02-27 20:48:49 +0100

    Opgave 4.1

---
+++ 04.7 Login/script.js
@@ -0,0 +1,20 @@
+
+let loggedInState = false;
+let user = null;
+
+function login(){
+    //TODO hent brugernavn og kodeord og check om login detaljer er korrekte (vælg selv brugernavn kodeord
+
+
+    //TODO hvis brugernavn/kodeord er korrekt - så opdater applikationstilstanden og kald updategui()
+
+}
+
+function updategui(){
+    if (loggedInState == true){
+        //TODO: opdater html-dokumentet til at skjule elementer der ikke skal vises når brugen er logget ind vises
+
+    } else {
+        //TODO: opdater html-dokumentet til at skjule elementer der ikke skal vises når loginbilledet vises
+    }
+}
