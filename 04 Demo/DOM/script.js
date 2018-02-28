b2b554380db4997294b958f190ebb3df8879c2a5
Christian Budtz <chrbudtz@gmail.com>
2018-02-28 11:15:41 +0100

    Demo 2

---
+++ 04 Demo/DOM/script.js
@@ -0,0 +1,5 @@
+//TODO read some text
+//alert(document.getElementById("input").innerText);
+//TODO write it back
+//document.getElementById("output").innerText= "Ny tekst!!";
+//TODO something more?
