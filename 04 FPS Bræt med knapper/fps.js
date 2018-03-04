c46a8763136c57b346e1067bb2425d7877856ec2
cbudtz <chrbudtz@gmail.com>
2018-03-04 17:57:18 +0100

    Opdatering til Bræt med knapper

--- 04 FPS Bræt med knapper/fps.js
+++ 04 FPS Bræt med knapper/fps.js
@@ -3,41 +3,51 @@
  */
 let context = document.getElementById("mycanvas").getContext('2d');
 
-let x = Number(prompt("Hvor skal der være en brik? Indtast X: "));
-let y = Number(prompt("Hvor skal der være en brik? Indtast Y: "));
+let columns = []; //TODO find på en måde at holde styr på brikkerne
 
-drawBoard(x,y);
+drawBoard();
 
 
-//TODO
+//Optegning af Boardet
 function drawBoard() {
     for(let i = 0; i<7;i++){
         for (let j=0;j<6;j++){
             drawCircle(25+i*53,25+j*53, 20);
         }
     }
+
 }
 
-//TODO
+//EventHandler for knapperne - colNo svarer til søjlens nummer
+function col(colNo){
+    //TODO - sæt en ny brik i kolonnenummer colNo
+
+
+}
+
+
+//------Tegne funktioner------
 function drawPieceAt(x, y, color) {
 
-
 }
 
-function drawCircle(x, y, r) {
-
-    context.beginPath();
-    context.arc(x,y,r,0,Math.PI*2);
-    context.stroke();
+function drawCircle(x, y, r, fill) {
+    //Fill the circle if fill is set
+    if (fill){
+        context.beginPath();
+        context.arc(x,y,r,0,Math.PI*2);
+        context.fillStyle = fill;
+        context.fill();
+    } else {
+        context.beginPath();
+        context.arc(x, y, r, 0, Math.PI * 2);
+        context.stroke();
+    }
 }
-
 
 
 //------------------
-//TODO Implementeres senere...
-function removePieceAt(x,y){
 
-}
 //TODO Implementeres senere...
 function showScore(player1Score, player2Score){
 
