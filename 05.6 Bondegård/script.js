1a5f7dd6bb1bf136836b7a9f258a95f03209f391
cbudtz <chrbudtz@gmail.com>
2018-03-04 17:39:09 +0100

    Oprydning

--- 05.6 Bondegård/script.js
+++ 05.6 Bondegård/script.js
@@ -14,7 +14,7 @@ animals.push(new Animal("cat", "Miuow"));
 //Print something interesting to the DOM
 for (let i = 0; i<animals.length;i++) {
     //adds a button with an onclick eventhandler that makes a sound!
-    document.getElementById("animaldiv").innerHTML +=
+    document.getElemmentById("animaldiv").innerHTML +=
         "<BR><button onclick='animals[" + i + "].makeSound()'>" + animals[i].type + "</button> "
 
 }
