3ec56facd63dd26305bac92581cf73ed447a7600
cbudtz <chrbudtz@gmail.com>
2018-03-11 12:18:07 +0100

    Løsning til nogle af sidste uges opgaver

---
+++ 05.3 Debugging/løsning/script.js
@@ -0,0 +1,13 @@
+
+let badeVand = [-1, 1, 1, 1, 2, 0, 1 ,-0.1, 0.1, -20, -4, 10, -10];
+
+let rises = 0;
+
+for (let i = 0; i+1 <badeVand.length; i++){
+    if (badeVand[i]<badeVand[i+1]){
+        rises++;
+    }
+
+}
+
+document.getElementById("output").innerHTML = "Antal stigninger: " + rises;
