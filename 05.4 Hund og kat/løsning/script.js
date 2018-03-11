44d36e81ca82c8368fa7d030d66a0824b225e0bd
cbudtz <chrbudtz@gmail.com>
2018-03-11 12:20:53 +0100

    Løsning til nogle af sidste uges opgaver

---
+++ 05.4 Hund og kat/løsning/script.js
@@ -0,0 +1,24 @@
+
+
+let dog = {
+    name: "Fido",
+    makeSound: function(){
+        document.getElementById("output").innerHTML = "WOOF"
+    }
+}
+
+let cat = {
+    name: "Garfield",
+    makeSound: function(){
+        document.getElementById("output").innerHTML = "MIAOW"
+    }
+}
+
+let goat = {
+    name: "Billy",
+    makeSound: function(){
+        document.getElementById("output").innerHTML = "BAAH"
+    }
+}
+
+//TODO Add another animal
