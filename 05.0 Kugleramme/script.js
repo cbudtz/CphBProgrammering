f64117e962ed2e572d752744a83a11fb862740a8
cbudtz <chrbudtz@gmail.com>
2018-11-02 19:35:24 +0100

    Flyttet applikationstilstands - opgave

--- 05.0 Kugleramme/script.js
+++ 05.0 Kugleramme/script.js
@@ -1,6 +1,7 @@
 //Applikationens tilstand
 let sum =0;
 
+//Funktioner der ændrer applikationens tilstand
 function addToSum(){
     let sumSpan = document.getElementById("sumSpan");
     //Opdater summen og vis den
